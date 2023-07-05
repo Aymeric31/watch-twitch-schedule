9f9c5e7b7ec5fb5c27a78fb57a5157faab57c0b4
Aymeric31 <aymeric.gioia@hotmail.fr>
2023-07-05 18:27:35 +0200

    send_notif.py updated: try except added on send_tweet, send_discord function added to post a message on discord server

--- sendnotif.py
+++ sendnotif.py
@@ -1,6 +1,7 @@
 import tweepy
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -10,20 +11,44 @@ twitter_consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
 twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
 twitter_access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 twitter_bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
+
+# Paramètres du serveur Discord
+discord_webhook = os.environ.get('DISCORD_WEBHOOK')
+
 twitch_username = os.environ.get('TWITCH_USERNAME')
+notification_message = f"Planning à jour ici ! ⬇️ https://www.twitch.tv/{twitch_username}/schedule"
 
-def send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text):
-    client = tweepy.Client( 
-        twitter_bearer_token, 
-        twitter_consumer_key, 
-        twitter_consumer_secret, 
-        twitter_access_token, 
-        twitter_access_token_secret, 
-        wait_on_rate_limit=True
-        )
-    # Envoi du tweet
-    client.create_tweet(text=tweet_text)
+def send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, notification_message):
+    try:
+        client = tweepy.Client( 
+            twitter_bearer_token, 
+            twitter_consumer_key, 
+            twitter_consumer_secret, 
+            twitter_access_token, 
+            twitter_access_token_secret, 
+            wait_on_rate_limit=True
+            )
+        # Envoi du tweet
+        client.create_tweet(text=notification_message)
+        print('Tweet envoyé avec succès.')
+    except Exception as e:
+        print('Erreur lors de l\'envoi du tweet :', str(e))
 
-tweet_text = f"Planning à jour ici ! ⬇️ https://www.twitch.tv/{twitch_username}/schedule"
+def send_discord(notification_message):
+    # Création du message à envoyer
+    message = {
+        'content': notification_message
+    }
 
-send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text)
+    # Envoi du message via la requête HTTP POST
+    response = requests.post(f"https://discord.com/api/webhooks/{discord_webhook}", json=message)
+
+    # Vérification du code de statut de la réponse
+    if response.status_code == 204:
+        print('Notification envoyée avec succès.')
+    else:
+        print('Échec de l\'envoi de la notification.')
+
+
+send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, notification_message)
+send_discord(notification_message)
